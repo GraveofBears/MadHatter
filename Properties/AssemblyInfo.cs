3913b149962d5b2d80e86b1a862cde6eb6a75cea
GraveofBears <83285287+GraveofBears@users.noreply.github.com>
2021-11-27 01:49:05 -0500

    fuck

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IronFangAxe")]
+[assembly: AssemblyTitle("MadHatter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IronFangAxe")]
+[assembly: AssemblyProduct("MadHatter")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
